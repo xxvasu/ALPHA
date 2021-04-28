b755fb7538b1b5214c8612183356be29e98110db
pro-vasu <79586348+pro-vasu@users.noreply.github.com>
2021-04-28 06:59:40 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -106,4 +106,4 @@ async def skip(_, message: Message):
 @errors
 async def admincache(client, message: Message):
     set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
-    #await message.reply_text("✯γ𝗎𝗄𝗂 ᴍᴜꜱɪᴄ✯=❇️ Admin cache refreshed!")
+    #await message.reply_text("✯ΛLPHA ᴍᴜꜱɪᴄ✯=❇️ Admin cache refreshed!")
