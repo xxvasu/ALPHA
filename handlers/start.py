34e8cc82ddb89145958a2314c9abf00640a19176
pro-vasu <79586348+pro-vasu@users.noreply.github.com>
2021-05-01 21:23:06 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -14,7 +14,7 @@ async def start_(client: Client, message: Message):
         f"""<b>Hi {message.from_user.first_name}!
 \nI can play music in your group's voice chat
 Maintained by @xxvasu ❤
-Any Query ➢ @shadow_viper
+
 \nTo add in your group contact us at @Yuki_Support
 \nHit /help list of available commands.
  </b>""",
